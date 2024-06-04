4713053b30be4457fd0e456edce110ddf7679389
Jesutoni <ojesutoni@gmail.com>
2024-06-04 23:18:33 +0100

    update changes

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import AchievementSection from './components/AchievementSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black-[#121212] ">
+    <main className="flex min-h-screen flex-col bg-primary ">
       <Navbar />
       <div className='container mt-24 mx-auto px-12 py-4'>
         <HeroSection />
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary': '#121212'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
