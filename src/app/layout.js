40bed05d260596c80fc76ebe442a79ced0737149
Jesutoni Odufeso <ojesutoni@gmail.com>
2025-05-19 10:00:54 +0100

    update metadata for improved portfolio description and SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,32 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'JayTee',
-  description: 'My Portfolio site containing my projects and where to contact me',
+  description: 'Welcome to my portfolio. Explore my projects in game development, web design, and creative coding.',
+  keywords: ['JayTee', 'JT', 'Portfolio', 'Game Developer', 'Web Designer'],
+  authors: [{ name: 'Jesutoni Odufeso' }],
+  creator: 'JayTee',
+  opengraph: {
+    title: 'JayTee',
+    description: 'Showcasing my best work in game and web development.',
+    url: 'https://jtsenpai.vercel.app',
+    siteName: 'JayTee',
+    images: [
+      {
+        url: '/images/preview.png',
+        width: 1200,
+        height: 630,
+        alt: 'JayTee Portfolio Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'JayTee',
+    description: 'Showcasing my best work in game and web development.',
+    images: ['/images/preview.png'],
+  }
 }
 
 export default function RootLayout({ children }) {
